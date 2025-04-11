ae6e710832d417bbd43aa9f9c5120d7756ec5d34
hoangn <1162050@gmail.com>
2025-04-11 17:51:39 +0700

    chore: add expiration time for redis

--- internal/utils/redis.go
+++ internal/utils/redis.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -33,7 +35,8 @@ func (c *CacheClient) Get(ctx context.Context, key string) (string, error) {
 }
 
 func (c *CacheClient) Set(ctx context.Context, key, value string) error {
-    return c.client.Set(ctx, key, value, 0).Err()
+	expiration, _ := strconv.Atoi(os.Getenv("REDIS_EXPIRATION"))
+    return c.client.Set(ctx, key, value, time.Duration(expiration)*time.Minute).Err()
 }
 
 func (c *CacheClient) Delete(ctx context.Context, key string) error {
